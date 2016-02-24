d2256767b2943832273a2fe04fb0dc3757d504c1
m3rc1l3ss <m3rc1l3ss@yahoo.com>
2016-02-24 16:06:56 -0600

    Base implementation upload
    Start screen added, along with a a framework for building the first
    level more or less,

---
+++ TitleScene.gd
@@ -0,0 +1,22 @@
+
+extends VBoxContainer
+
+# member variables here, example:
+# var a=2
+# var b="textvar"
+
+func _ready():
+	# Initialization here
+	pass
+
+
+
+
+func _on_NewGame_pressed():
+
+	pass # replace with function body
+
+
+func _on_QuitButton_pressed():
+	get_tree().quit()
+	pass # replace with function body
